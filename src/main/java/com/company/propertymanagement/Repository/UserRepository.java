1584acfa2c465fe54e2797eb3c52b460dac3d76a
MP <stpln@tutanota.com>
2023-11-21 13:54:06 +0100

    User login functionality

--- src/main/java/com/company/propertymanagement/Repository/UserRepository.java
+++ src/main/java/com/company/propertymanagement/Repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.company.propertymanagement.Repository;
 import com.company.propertymanagement.Entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+    Optional<UserEntity> findByOwnerEmailAndPassword(String email, String password);
 }
