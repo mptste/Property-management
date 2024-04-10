84616db9eb31f02f0145aaa8538876a0416a7a2b
mptste <zbnhl@tutanota.com>
2024-04-10 14:42:28 +0200

    Preparation for JWT

---
+++ src/main/java/com/company/propertymanagement/entity/Role.java
@@ -0,0 +1,9 @@
+package com.company.propertymanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+}
---
+++ src/main/java/com/company/propertymanagement/enums/URole.java
@@ -0,0 +1,8 @@
+package com.company.propertymanagement.enums;
+
+public enum URole {
+
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MOD
+}
